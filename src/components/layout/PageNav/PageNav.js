0ffb914bb1d1b68337b82c94161f71bdccedb04f
Adam Pietrzak <adampietrzak@Adams-MacBook-Air.local>
2021-05-13 18:08:35 +0200

    Correction PageNav , not working

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -6,8 +6,15 @@ import Button from '@material-ui/core/Button';
 const PageNav = () => (
   <nav className={styles.component}>
     <Button className={styles.link} component = {NavLink} exact to={`${process.env.PUBLIC_URL}/`} activeClassName='active'>Dashboard</Button>
-    <Button className={styles.link} component = {NavLink} to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Button>
+    <Button className={styles.link} component={React.forwardRef (
+      (props, ref) => (
+        <NavLink to={`${process.env.PUBLIC_URL}/login`} activeClassName='active' {...props} ref={ref} />
+      )
+    )} >Login</Button>
   </nav>
 );
 
 export default PageNav;
+
+
+
