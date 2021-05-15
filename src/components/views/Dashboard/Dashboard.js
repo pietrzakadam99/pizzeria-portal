9fe0ab351c270d1a63a19762bfb84db096517903
Adam Pietrzak <adampietrzak@Adams-MacBook-Air.local>
2021-05-15 13:26:15 +0200

    correction, not working

--- src/components/views/Dashboard/Dashboard.js
+++ src/components/views/Dashboard/Dashboard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from '../Dashboard/Dashboard.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Dashboard = () => (
     <div className={styles.component}>
         <h2>Dashboard view</h2>
-        <Link to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</Link>
-        <Link to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</Link>
-        <Link to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</Link>
-        <Link to={`${process.env.PUBLIC_URL}/ordering`} activeClassName='active'>Ordering</Link>
+        <NavLink to={`${process.env.PUBLIC_URL}/kitchen`} activeClassName='active'>Kitchen</NavLink>
+        <NavLink to={`${process.env.PUBLIC_URL}/login`} activeClassName='active'>Login</NavLink>
+        <NavLink to={`${process.env.PUBLIC_URL}/tables`} activeClassName='active'>Tables</NavLink>
+        <NavLink to={`${process.env.PUBLIC_URL}/ordering`} activeClassName='active'>Ordering</NavLink>
     </div>
 )
 
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from '../Tables/Tables.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Tables = () => (
     <div className={styles.component}>
         <h2>Tables view</h2>
-        <Link to={`${process.env.PUBLIC_URL}/tables/booking/123abc`} activeClassName='active'>Booking</Link>
-        <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`} activeClassName='active'>New booking</Link>
-        <Link to={`${process.env.PUBLIC_URL}/tables/events/123abc`} activeClassName='active'>Events</Link>
-        <Link to={`${process.env.PUBLIC_URL}/tables/events/new`} activeClassName='active'>New event</Link>
+        <NavLink to={`${process.env.PUBLIC_URL}/tables/booking/123abc`} activeClassName='active'>Booking</NavLink>
+        <NavLink to={`${process.env.PUBLIC_URL}/tables/booking/new`} activeClassName='active'>New booking</NavLink>
+        <NavLink to={`${process.env.PUBLIC_URL}/tables/events/123abc`} activeClassName='active'>Events</NavLink>
+        <NavLink to={`${process.env.PUBLIC_URL}/tables/events/new`} activeClassName='active'>New event</NavLink>
     </div>
 )
 
